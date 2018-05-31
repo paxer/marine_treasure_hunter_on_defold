6b70a9450ea52708a3cc5a70097e981ec19190cb
Pavel Kotlyar <pavel.kotlyar@icloud.com>
2018-05-31 21:55:33 +1000

    make player hose work

--- game/objects/hose_segment.go
+++ game/objects/hose_segment.go
@@ -1,5 +1,20 @@
-embedded_components {
+components {
   id: "hose_segment"
+  component: "/game/scripts/hose_segment.script"
+  position {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+  }
+  rotation {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+    w: 1.0
+  }
+}
+embedded_components {
+  id: "sprite"
   type: "sprite"
   data: "tile_set: \"/data/sprites.atlas\"\n"
   "default_animation: \"hose_segment\"\n"
