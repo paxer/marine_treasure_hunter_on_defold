e6adf28e5700610501c6c3678353692a3eb20610
Pavel Kotlyar <pavel.kotlyar@icloud.com>
2018-09-03 18:36:02 +1000

    add collisions to crab, player and bullet objects

--- game/modules/crab_spawner.lua
+++ game/modules/crab_spawner.lua
@@ -9,7 +9,7 @@ local left_side_screen_spawn_x = -64
 local x_start_positions = { left_side_screen_spawn_x, right_side_screen_spawn_x } -- could start from left or right screen side
 local x_direction
 
-create_crab = function()
+local create_crab = function()
 	local x_start_position = x_start_positions[math.random(1, 2)]
 	if x_start_position == left_side_screen_spawn_x then
 		x_direction = 1
@@ -18,9 +18,8 @@ create_crab = function()
 	end
 	local pos = vmath.vector3(x_start_position, 80, 1)
 	local dir = vmath.vector3(x_direction, 0, 0)
-	local speed = math.random(50, 100)
+	local speed = math.random(20, 50)
 	local props = { dir = dir, speed = speed }
-	
 	factory.create("#crab_factory", pos, nil, props)
 end
 
