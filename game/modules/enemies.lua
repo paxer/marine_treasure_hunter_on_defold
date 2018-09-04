0f791bb281dbc850266b63e98db8d7d24e45d3c2
Pavel Kotlyar <pavel.kotlyar@icloud.com>
2018-09-04 22:01:22 +1000

    finish Fish enemy behavior

---
+++ game/modules/enemies.lua
@@ -0,0 +1,25 @@
+-- Put functions in this file to use them in several other scripts.
+-- To get access to the functions, you need to put:
+-- require "my_directory.my_file"
+-- in any script using the functions.
+
+local M = {}
+
+function M.handle_bullets_hit(message_id, message)
+	if message_id == hash("trigger_response") then
+		if message.enter then
+			if message.other_group == hash("bullets") then
+				msg.post("main:/sfx_library#enemy_hit", "play_sound")
+				go.delete()
+			end
+		end
+	end
+end
+
+function M.handle_is_paused(self, message_id)
+	if message_id == hash("pause") then
+		self.is_paused = true
+	end
+end
+
+return M
