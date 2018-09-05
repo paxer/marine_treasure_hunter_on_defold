1c480dd073495f4acb90e11b4a0f25e2ba2733d4
Pavel Kotlyar <pavel.kotlyar@icloud.com>
2018-09-05 18:49:27 +1000

    add inital chest spawning

---
+++ game/modules/treasures_spawner.lua
@@ -0,0 +1,23 @@
+-- Put functions in this file to use them in several other scripts.
+-- To get access to the functions, you need to put:
+-- require "my_directory.my_file"
+-- in any script using the functions.
+local M = {}
+
+local left_side_screen_spawn_x = 64
+local right_side_screen_spawn_x = tonumber(sys.get_config("display.width") - 64)
+local y_spawn = 87
+
+local create_chest = function()
+	local x = math.random(left_side_screen_spawn_x, right_side_screen_spawn_x)
+	local pos = vmath.vector3(x, y_spawn, 1)
+	--local dir = vmath.vector3(x_direction, 0, 0)
+	factory.create("#chest_factory", pos, nil, nil)
+end
+
+function M.spawn_treasures()
+	math.randomseed(os.clock() * 100000000000)
+	create_chest()
+end
+
+return M
