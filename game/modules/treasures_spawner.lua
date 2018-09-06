5591a75639311bb5734a70032fcf94c519144cdd
Pavel Kotlyar <pavel.kotlyar@icloud.com>
2018-09-06 21:43:18 +1000

    add chest animation

--- game/modules/treasures_spawner.lua
+++ game/modules/treasures_spawner.lua
@@ -17,10 +17,19 @@ local spawn_area3 = {start_point = spawn_area2.end_point, end_point = display_wi
 local spawn_areas = { spawn_area1, spawn_area2, spawn_area3 }
 local y_spawn = 87
 
+local play_chest_animation = function(chest)
+	local animations = { "chest_1", "chest_2", "chest_3" }
+	msg.post(chest, "play_animation", {id = hash(utils.shuffle(animations)[1])})	
+end
+
 local create_chest = function(spawn_area)
 	local x = math.random(spawn_area.start_point, spawn_area.end_point)
 	local pos = vmath.vector3(x, y_spawn, 1)
-	factory.create("#chest_factory", pos)
+	local chest = factory.create("#chest_factory", pos)
+	
+	timer.delay(math.random(5, 10), true, function()
+		play_chest_animation(chest)
+	end)
 end
 
 local create_gold = function(spawn_area)
